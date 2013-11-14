04b5befc0cacf42715f0341a05a8129170ae5552
Rob Hoffman <hoffmanrob@yahoo.com>
2013-11-14 23:12:09 +0000

    Updating to new environment variables.

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -x
 
-## This script will start the we-services application.
+## This script will start the wcrs-services application.
 ## Please refer to README.deploy for futher details.
 
 function env_alert() {
@@ -15,44 +15,44 @@ function env_alert() {
 echo ""
 
 ## Ensure required environment variables have been set.
-if [[ -z "${WESERVICES_JAVA_HOME}" ]]; then env_alert WESERVICES_JAVA_HOME; fi
-if [[ -z "${WESERVICES_HOME}" ]]; then env_alert WESERVICES_HOME; fi
-if [[ -z "${WESERVICES_PORT}" ]]; then env_alert WESERVICES_PORT; fi
+if [[ -z "${WCRS_SERVICES_JAVA_HOME}" ]]; then env_alert WCRS_SERVICES_JAVA_HOME; fi
+if [[ -z "${WCRS_SERVICES_HOME}" ]]; then env_alert WCRS_SERVICES_HOME; fi
+if [[ -z "${WCRS_SERVICES_PORT}" ]]; then env_alert WCRS_SERVICES_PORT; fi
 
 ## Ensure directory structure is in place.
-if [ -d "${WESERVICES_HOME}" ]; then
-  if [ ! -w "${WESERVICES_HOME}/live/logs" ]; then
-    echo "ERROR: Unable to write to ${WESERVICES_HOME}/live/logs. Exiting now."
+if [ -d "${WCRS_SERVICES_HOME}" ]; then
+  if [ ! -w "${WCRS_SERVICES_HOME}/live/logs" ]; then
+    echo "ERROR: Unable to write to ${WCRS_SERVICES_HOME}/live/logs. Exiting now."
     exit 1
   fi
 else
-  echo "ERROR: ${WESERVICES_HOME} does not exist."
+  echo "ERROR: ${WCRS_SERVICES_HOME} does not exist."
   exit 1
 fi
 
 
 ## Verify that we-services isn't currently running.
-WESERVICES_PID=`ps -ef | grep java | grep ${WESERVICES_PORT} | awk '{print $2}'`
-if [[ ! -z "${WESERVICES_PID}" ]]; then
-  echo "we-services is already running or some other daemon is already using port ${WESERVICES_PORT}."
+WESERVICES_PID=`ps -ef | grep java | grep ${WCRS_SERVICES_PORT} | awk '{print $2}'`
+if [[ ! -z "${WCRS_SERVICES_PID}" ]]; then
+  echo "wcrs-services is already running or some other daemon is already using port ${WCRS_SERVICES_PORT}."
   echo ""
   exit 0
 fi
 
 
 ## Use the jar file with the most recent timestamp.
-WESERVICES_JAR=`ls -tr "${WESERVICES_HOME}/live/webapps/" | grep '\<waste-exemplar-services.*jar\>' | tail -1`
+WESERVICES_JAR=`ls -tr "${WCRS_SERVICES_HOME}/live/webapps/" | grep '\<waste-exemplar-services.*jar\>' | tail -1`
 
 
 ## Start we-services.
-echo "Starting we-services on port ${WESERVICES_PORT}."
-cd "${WESERVICES_HOME}/live/logs"
-if [ -f "${WESERVICES_HOME}/live/logs/nohup.out" ]; then
+echo "Starting wcrs-services on port ${WCRS_SERVICES_PORT}."
+cd "${WCRS_SERVICES_HOME}/live/logs"
+if [ -f "${WCRS_SERVICES_HOME}/live/logs/nohup.out" ]; then
   DATESTAMP=`date +%Y.%m.%d-%H.%M`
   mv nohup.out nohup.out.${DATESTAMP}
 fi
-nohup "${WESERVICES_JAVA_HOME}/bin/java" -Ddw.http.port=${WESERVICES_PORT} -jar "${WESERVICES_HOME}/live/webapps/${WESERVICES_JAR}" server "${WESERVICES_HOME}/live/conf/configuration.yml" &
-echo $! > "${WESERVICES_HOME}/live/logs/pid"
+nohup "${WCRS_SERVICES_JAVA_HOME}/bin/java" -Ddw.http.port=${WCRS_SERVICES_PORT} -jar "${WCRS_SERVICES_HOME}/live/webapps/${WCRS_SERVICES_JAR}" server "${WCRS_SERVICES_HOME}/live/conf/configuration.yml" &
+echo $! > "${WCRS_SERVICES_HOME}/live/logs/pid"
 
 echo ""
 exit 0
