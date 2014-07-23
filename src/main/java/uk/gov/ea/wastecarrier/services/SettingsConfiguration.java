e1df95b151f2c23096c4e3362e4bfcf1043a9d1a
steven-radford <stevenr@aptosolutions.co.uk>
2014-07-23 16:34:25 +0100

    update settings validation

--- src/main/java/uk/gov/ea/wastecarrier/services/SettingsConfiguration.java
+++ src/main/java/uk/gov/ea/wastecarrier/services/SettingsConfiguration.java
@@ -1,5 +1,8 @@
 package uk.gov.ea.wastecarrier.services;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import com.yammer.dropwizard.config.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -14,11 +17,13 @@ public class SettingsConfiguration extends Configuration {
     @JsonProperty
     private String registrationRenewPeriod = "6 MONTHS";
     
-    @NotEmpty
+    @Min(1)
+    @Max(100000000)
     @JsonProperty
     private int registrationCost = 15400;
     
-    @NotEmpty
+    @Min(1)
+    @Max(100000000)
     @JsonProperty
     private int copyCardCost = 1000;
 
