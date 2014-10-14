2e62e79608448f612cc0d794fdfc574031108cec
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2014-10-14 12:18:22 +0100

    Updated reIndex to pull through SERVICES_ADMIN_PORT and not just the SERVICES_PORT.

--- bin/reIndex.sh
+++ bin/reIndex.sh
@@ -25,21 +25,21 @@ echo "TODO esPort: No ENV var set, using default 9200"
 
 wcrsServicesName="localhost"
 echo "TODO wcrsServicesName: No ENV var set, using default localhost"
-wcrsServicesPort=${WCRS_SERVICES_PORT}
-if [ $wcrsServicesPort ]
+wcrsServicesAdminPort=${WCRS_SERVICES_ADMIN_PORT}
+if [ $wcrsServicesAdminPort ]
 then
   echo "String length not zero"
 else
-  echo "Using local setting for WCRS_SERVICES_PORT"
-  wcrsServicesPort="9091"
+  echo "Using local setting for WCRS_SERVICES_ADMIN_PORT"
+  wcrsServicesAdminPort="9091"
 fi
-echo "wcrsServicesPort: $wcrsServicesPort"
+echo "wcrsServicesAdminPort: $wcrsServicesAdminPort"
 
 echo "Generate XY Coordinates"
-curl -X POST http://$wcrsServicesName:$wcrsServicesPort/tasks/location
+curl -X POST http://$wcrsServicesName:$wcrsServicesAdminPort/tasks/location
 
 echo "Delete all Elastic Search indexes"
-curl -X POST http://$wcrsServicesName:$wcrsServicesPort/tasks/indexer -d 'deleteAll'
+curl -X POST http://$wcrsServicesName:$wcrsServicesAdminPort/tasks/indexer -d 'deleteAll'
 
 echo "Create a clean registrations index"
 curl -X POST http://$esHost:$esPort/registrations
@@ -48,7 +48,7 @@ echo "Set the index structure in Elastic Search"
 curl -X PUT http://$esHost:$esPort/registrations/registration/_mapping -d '@registration_mapping.json'
 
 echo "Re-populate the elastic search index"
-curl -X POST http://$wcrsServicesName:$wcrsServicesPort/tasks/indexer
+curl -X POST http://$wcrsServicesName:$wcrsServicesAdminPort/tasks/indexer
 
 echo "End Re-Index"
 
