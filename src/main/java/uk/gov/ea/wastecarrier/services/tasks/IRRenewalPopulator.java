41b6b03a7db812e01faf8f8d777e23b98857f37e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-03-21 22:00:30 +0000

    Followed advice of Intellij

--- src/main/java/uk/gov/ea/wastecarrier/services/tasks/IRRenewalPopulator.java
+++ src/main/java/uk/gov/ea/wastecarrier/services/tasks/IRRenewalPopulator.java
@@ -10,6 +10,7 @@ import uk.gov.ea.wastecarrier.services.mongoDb.IRRenewalMongoDao;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 /**
@@ -111,7 +112,7 @@ public class IRRenewalPopulator extends Task
 		try {
 			int count = 0;
 			reader = new CSVReader(new FileReader(csvFile));
-            log.info(reader.readNext().toString());
+            log.info(Arrays.toString(reader.readNext()));
 			while ((irDataRow = reader.readNext()) != null) {
 				// use comma as separator
                 if (irDataRow.length < 4) {
