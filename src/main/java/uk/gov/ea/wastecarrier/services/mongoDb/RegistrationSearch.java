afe8174663a17eeada0cfbead09e7ae8d8434636
MJ2P <mj2p.sg@gmail.com>
2015-04-01 15:34:57 +0100

    changes to allow for searching of copy cards from reporting page

--- src/main/java/uk/gov/ea/wastecarrier/services/mongoDb/RegistrationSearch.java
+++ src/main/java/uk/gov/ea/wastecarrier/services/mongoDb/RegistrationSearch.java
@@ -16,6 +16,7 @@ public class RegistrationSearch {
     private final static String DATE_FILTER_PROPERTY = "metaData.dateRegistered";
     private final static String COMPANY_CONVICTION_MATCH = "convictionSearchResult.matchResult";
     private final static String KEY_PEOPLE_CONVICTION_MATCH = "keyPeople.convictionSearchResult.matchResult";
+    private final static String COPY_CARDS_MATCH = "financeDetails.orders.orderItems.type";
 
     private SearchHelper searchHelper;
 	private Logger log = Logger.getLogger(RegistrationSearch.class.getName());
@@ -26,6 +27,7 @@ public class RegistrationSearch {
 	public Set<String> status;
 	public Set<String> businessType;
     public Set<String> tier;
+    public Set<String> copyCards;
     public Optional<String> declaredConvictions;
     public Optional<String> convictionCheckMatch;
     public Optional<Integer> resultCount;
@@ -48,7 +50,15 @@ public class RegistrationSearch {
             for (Map.Entry<String, Object> entry : queryProps.entrySet()) {
                 Object value = entry.getValue();
                 if (value instanceof String[]) {
-                    BasicDBObject inQuery = new BasicDBObject("$in", value);
+                    String key = entry.getKey();
+                    BasicDBObject inQuery;
+                    //Sam Griffiths 01-04-2015 check here for Copy Cards requests.
+                    // requires an $all request instead of an $in
+                    if (key.equals(COPY_CARDS_MATCH)) {
+                        inQuery = new BasicDBObject("$all", value);
+                    } else {
+                        inQuery = new BasicDBObject("$in", value);
+                    }
                     query.append(entry.getKey(), inQuery);
                 } else {
                     query.append(entry.getKey(), value);
@@ -135,6 +145,18 @@ public class RegistrationSearch {
         searchHelper.addOptionalQueryProperty("metaData.route", this.route, queryProps);
         searchHelper.addOptionalQueryProperty("businessType", this.businessType, queryProps);
         searchHelper.addOptionalQueryProperty("tier", this.tier, queryProps);
+
+        //Sam Griffiths add Copy Cards search
+        if (!copyCards.isEmpty()) {
+            //if the any selection has been made it overrides the others and just returns all instances of copy cards
+            if (copyCards.contains("ANY")) {
+                copyCards.clear();
+            }
+            copyCards.add("COPY_CARDS");
+        }
+        searchHelper.addOptionalQueryProperty(COPY_CARDS_MATCH, this.copyCards, queryProps);
+        log.info("\n\ncopyCards = " + this.copyCards.toString() + "\n\n");
+        log.info("\n\nqueryProps = " + queryProps.toString() + "\n\n");
 
 		return queryProps;
 	}
--- src/main/java/uk/gov/ea/wastecarrier/services/mongoDb/SearchHelper.java
+++ src/main/java/uk/gov/ea/wastecarrier/services/mongoDb/SearchHelper.java
@@ -230,4 +230,5 @@ public class SearchHelper {
 
         return result;
     }
+
 }
