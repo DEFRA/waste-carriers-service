db7d17074df12faa7979a3a626c0c9dcfc27e319
Tom Thurston <tomthurstonwork@live.com>
2016-03-07 11:51:06 +0000

    Update shell scripts in the "bin" directory to reflect the updated indexer command, and also that we no longer use the output of the "location" task.

--- bin/reindex_elasticsearch.sh
+++ bin/reindex_elasticsearch.sh
@@ -1,49 +1,19 @@
-#!/bin/bash
-#set -x
+#!/usr/bin/env bash
 
-## Make it possible to use the public search against existing records in the database.
-## wcrs-services must be running. These commands should be run on an app server.
+## Drop and re-create the "registrations" index in ElasticSearch, then re-index
+## all registrations in the Mongo database.
 
-## This script should only be started by the user wcrs-services.
-USER=`/usr/bin/whoami`
-if [ "$USER" != "wcrs-services" ]; then
-    echo "ERROR: This script should only be run by the user wcrs-services."
+if [[ -z "${WCRS_SERVICES_ADMIN_PORT}" ]]; then
+    echo "Environment variable WCRS_SERVICES_ADMIN_PORT is not set"
     exit 1
 fi
 
-if [[ -z "${WCRS_SERVICES_DB_HOST}" ]]; then
-  echo "ERROR: The environment variable WCRS_SERVICES_DB_HOST is undefined."
-  echo ""
-  exit 1
+if [[ ! -e "registration_mapping.json" ]]; then
+    echo "Can't find registration_mapping.json"
+    echo "Make sure you run this script from within the bin directory"
+    exit 1
 fi
 
-## Display a little white space before we begin.
-echo ""
-
-echo "Create accurate XY coordinates in the database for each postcode provided."
-curl --ipv4 -X POST http://localhost:9091/tasks/location
-echo ""
-
-echo "Delete existing Elastic search records. It may give an exception about"
-echo "registrations not found, this should be safe to ignore."
-echo "Note: This will throw an error the first time it's run because the index doesn't exist yet."
-curl --ipv4 -X POST http://localhost:9091/tasks/indexer -d 'deleteAll'
-echo ""
-
-echo "Recreate the empty registrations index."
-curl --ipv4 -X POST http://$WCRS_SERVICES_DB_HOST:9200/registrations
-echo ""
-
-echo "Set a custom mapping for the registration object."
-curl --ipv4 -X PUT $WCRS_SERVICES_DB_HOST:9200/registrations/registration/_mapping -d '@registration_mapping.json'
-echo ""
-
-echo "Re-index the data from the database (without any parameters)"
-curl --ipv4 -X POST http://localhost:9091/tasks/indexer
-echo ""
-
-echo "Re-indexing is now complete."
-echo ""
+curl --ipv4 -X POST http://localhost:${WCRS_SERVICES_ADMIN_PORT}/tasks/indexer -d '@registration_mapping.json'
 
 exit 0
-
