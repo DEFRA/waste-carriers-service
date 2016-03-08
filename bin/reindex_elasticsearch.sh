7595fb4c514bfbff2ea0edda08df80c2a9d17b04
Tom Thurston <tomthurstonwork@live.com>
2016-03-08 16:43:51 +0000

    Update the "reindex_elasticsearch" shell script so that it makes registrations searchable via postcode.

--- bin/reindex_elasticsearch.sh
+++ bin/reindex_elasticsearch.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-## Drop and re-create the "registrations" index in ElasticSearch, then re-index
-## all registrations in the Mongo database.
+## Make all registrations searchable via postcode, then drop and re-create the
+## "registrations" index in ElasticSearch, then re-index all registrations in
+## the Mongo database.
 
 if [[ -z "${WCRS_SERVICES_ADMIN_PORT}" ]]; then
     echo "Environment variable WCRS_SERVICES_ADMIN_PORT is not set"
@@ -14,6 +15,12 @@ if [[ ! -e "registration_mapping.json" ]]; then
     exit 1
 fi
 
+# First, make records searchable via postcode.
+echo "Adding post-code lookup information"
+curl --ipv4 -X POST http://localhost:${WCRS_SERVICES_ADMIN_PORT}/tasks/location
+
+# Then index all registrations in Elastic Search.
+echo "Indexing registrations in Elastic Search"
 curl --ipv4 -X POST http://localhost:${WCRS_SERVICES_ADMIN_PORT}/tasks/indexer -d '@registration_mapping.json'
 
 exit 0
