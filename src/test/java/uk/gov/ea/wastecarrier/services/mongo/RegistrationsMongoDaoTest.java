79047f49515942c7bfbea1282dfa695823330c08
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-04-16 09:56:02 +0100

    Expand current MongoDb test coverage
    We wanted to provide support for quickly testing that the service can connect and manage data in MongoDb. So we have gone with keeping in the existing tests to check the connection and that we can create a registration, and expanded them to include find and update.
    
    We have also updated the test class to use the annotation format (which is the current model of doing JUnit tests) and to support all the tests whilst avoiding duplication configured them to run in a specific order with a clean down for the registration created.
    
    However to avoid needing to have an environment where MongoDb, the databases and the collections exist, we have configured the pom so the tests are no longer run during a package. They now need to be invoked manually, but this fits better with there intended purpose of checking the services can connect rather than that the code is behaving as expected.

--- src/test/java/uk/gov/ea/wastecarrier/services/mongo/RegistrationsMongoDaoTest.java
+++ src/test/java/uk/gov/ea/wastecarrier/services/mongo/RegistrationsMongoDaoTest.java
@@ -1,17 +1,14 @@
-/**
- * 
- */
 package uk.gov.ea.wastecarrier.services.mongo;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import junit.framework.TestCase;
-import org.bson.Document;
-import org.mongojack.DBCursor;
-import org.mongojack.DBQuery;
-import org.mongojack.JacksonDBCollection;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.mongojack.WriteResult;
 import uk.gov.ea.wastecarrier.services.DatabaseConfiguration;
 import uk.gov.ea.wastecarrier.services.core.*;
 import uk.gov.ea.wastecarrier.services.core.MetaData.RegistrationStatus;
@@ -21,29 +18,32 @@ import uk.gov.ea.wastecarrier.services.mongoDb.OrdersMongoDao;
 import uk.gov.ea.wastecarrier.services.mongoDb.PaymentsMongoDao;
 import uk.gov.ea.wastecarrier.services.mongoDb.RegistrationsMongoDao;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Date;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests (involving the MongoDB database) for the Registrations DAO.
- * @author gmueller
- *
  */
-public class RegistrationsMongoDaoTest extends TestCase {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RegistrationsMongoDaoTest {
 
-	private RegistrationsMongoDao dao;
-	
-	private OrdersMongoDao ordersDao;
-	
-	private PaymentsMongoDao paymentsDao;
-	
-	/**
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	//TODO RE-enable after variables have been set up on the Jenkins server
-	protected void setUp() throws Exception {
-		//TODO Get config from DW config files
-		String host = System.getenv("WCRS_SERVICES_DB_HOST");
+	private static RegistrationsMongoDao dao;
+	private static OrdersMongoDao ordersDao;
+	private static PaymentsMongoDao paymentsDao;
+	private static DatabaseHelper helper;
+
+	private static final String lowerTierRegNumber = "CBDL99999";
+
+	@BeforeClass
+    public static void runOnceBefore() {
+	    String host = System.getenv("WCRS_SERVICES_DB_HOST");
         int port = Integer.valueOf(System.getenv("WCRS_SERVICES_DB_PORT"));
 		String name = System.getenv("WCRS_SERVICES_DB_NAME");
 		String username = System.getenv("WCRS_SERVICES_DB_USER");
@@ -51,38 +51,82 @@ public class RegistrationsMongoDaoTest extends TestCase {
 
 		DatabaseConfiguration config = new DatabaseConfiguration(host, port, name, username, password);
 
-		this.dao = new RegistrationsMongoDao(config);
-		
-		this.ordersDao = new OrdersMongoDao(config);
-		
-		this.paymentsDao = new PaymentsMongoDao(config);
+		helper = new DatabaseHelper(config);
+		dao = new RegistrationsMongoDao(config);
+		ordersDao = new OrdersMongoDao(config);
+		paymentsDao = new PaymentsMongoDao(config);
 	}
 
-	/**
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
+    /**
+     * Deletes any registrations we have created during testing
+     * @throws Exception
+     */
+	@AfterClass
+    public static void runOnceAfter() {
+		DB db = helper.getConnection();
+        DBCollection collection = db.getCollection("registrations");
+
+        BasicDBObject lowerTierQuery = new BasicDBObject("regIdentifier", lowerTierRegNumber);
+        collection.remove(lowerTierQuery);
 	}
 
-	/**
-	 * Test method for {@link uk.gov.ea.wastecarrier.services.mongoDb.RegistrationsMongoDao#getNumberOfRegistrationsTotal()}.
-	 */
-	//TODO RE-enable tests
-	public void testGetNumberOfRegistrationsTotal() {
-		long total = dao.getNumberOfRegistrationsTotal();
-		System.out.println(total);
-        System.out.println("Test complete: GetNumberOfRegistrationsTotal");
+    /**
+     * Check we can connect to MongoDb. Simply creating a connection is not a
+     * guarantee that everything is working. So we also call
+     * getNumberOfRegistrationsTotal() as this performs a query against the DB,
+     * and checks we can connect, authenticate, query and return a result
+     */
+	@Test
+	public void test1_getNumberOfRegistrationsTotal() {
+		dao.getNumberOfRegistrationsTotal();
 	}
 
-	/**
-	 * Test method for {@link uk.gov.ea.wastecarrier.services.mongoDb.RegistrationsMongoDao#getNumberOfRegistrationsPending()}.
-	 */
-	//TODO Re-enable tests
-	public void doNotTestGetNumberOfRegistrationsPending() {
-		//long total = dao.getNumberOfRegistrationsPending();
-		//System.out.println("total = " + total);
-	}
+    /**
+     * Check we can insert a new lower tier registration into the registrations
+     * collection. This goes beyond simply connecting and querying, and
+     * checks that the Monjojack POJO mapping is working correctly.
+     */
+    @Test
+    public void test2_insertRegistration() {
+        Registration reg = dao.insertRegistration(generateLowerTier());
+        String id = reg.getId();
+        assertTrue("The registration is inserted", id != null && !id.isEmpty());
+    }
+
+    /**
+     * Check we can find our registration
+     */
+    @Test
+    public void test3_findRegistration() {
+        Registration reg = dao.findRegistration(lowerTierRegNumber);
+        String regNo = reg.getRegIdentifier();
+        assertEquals("The registration is found", lowerTierRegNumber, regNo);
+    }
+
+    /**
+     * Check we can update our registration
+     */
+    @Test
+    public void test4_updateRegistration() {
+        Registration reg = dao.findRegistration(lowerTierRegNumber);
+        reg.getMetaData().setStatus(RegistrationStatus.ACTIVE);
+        reg.getMetaData().setDateActivated(new Date());
+        reg = dao.updateRegistration(reg);
+        assertEquals(
+                "The registration is updated",
+                RegistrationStatus.ACTIVE,
+                reg.getMetaData().getStatus()
+        );
+    }
+
+    /**
+     * Test method for {@link uk.gov.ea.wastecarrier.services.mongoDb.RegistrationsMongoDao#getNumberOfRegistrationsPending()}.
+     */
+    //TODO Re-enable tests
+    public void doNotTestGetNumberOfRegistrationsPending() {
+        //long total = dao.getNumberOfRegistrationsPending();
+        //System.out.println("total = " + total);
+    }
 	
 	//TODO Re-enable tests
 	public void doNotTestInsertAndGetRegistration()
@@ -179,5 +223,100 @@ public class RegistrationsMongoDaoTest extends TestCase {
 		reg.getMetaData().setRoute(RouteType.DIGITAL);
 		return reg;
 	}
+
+	private Registration generateLowerTier() {
+	    Registration reg = new Registration();
+
+	    reg.setUuid(generateUUID());
+	    reg.setRegIdentifier(lowerTierRegNumber);
+	    reg.setTier(Registration.RegistrationTier.LOWER);
+	    reg.setBusinessType("soleTrader");
+	    reg.setOtherBusinesses("no");
+	    reg.setConstructionWaste("no");
+	    reg.setCompanyName("WCR Service test LT");
+	    reg.setFirstName("Jason");
+	    reg.setLastName("Isaac");
+	    reg.setPhoneNumber("01179345400");
+	    reg.setContactEmail("jason@example.com");
+	    reg.setAccountEmail("jason@example.com");
+	    reg.setDeclaration("1");
+
+        ArrayList<Address> addresses = new ArrayList<Address>();
+        addresses.add(generateAddress(Address.addressType.REGISTERED));
+        addresses.add(generateAddress(Address.addressType.POSTAL));
+        reg.setAddresses(addresses);
+
+        reg.setMetaData(generateMetaData(RegistrationStatus.PENDING));
+
+        reg.setFinanceDetails(generateFinanceDetails());
+
+	    return reg;
+    }
+
+    private Address generateAddress(Address.addressType type) {
+	    Address addr = new Address();
+
+	    addr.setAddressType(type);
+        addr.setHouseNumber("123");
+        addr.setAddressLine1("Upper Street");
+        addr.setTownCity("Bristol");
+        addr.setPostcode("BS1 5AH");
+
+        if (type == Address.addressType.POSTAL) {
+            addr.setFirstName("Jason");
+            addr.setLastName("Isaac");
+        }
+
+        return addr;
+    }
+
+    private MetaData generateMetaData(RegistrationStatus status) {
+	    MetaData data = new MetaData();
+
+        data.setRoute(RouteType.DIGITAL);
+        data.setDateRegistered(new Date());
+        data.setLastModified(data.getDateRegistered());
+
+	    if (status == RegistrationStatus.ACTIVE) {
+            data.setStatus(RegistrationStatus.PENDING);
+            data.setDateActivated(new Date());
+        } else {
+            data.setStatus(RegistrationStatus.PENDING);
+        }
+
+	    return data;
+    }
+
+    private FinanceDetails generateFinanceDetails() {
+	    FinanceDetails details = new FinanceDetails();
+        Order order = new Order();
+        order.setOrderCode(Long.toString(new Date().getTime()));
+        order.setOrderCode(order.getOrderId());
+        order.setPaymentMethod(Order.PaymentMethod.ONLINE);
+        order.setMerchantId("EASERRSIMECOM");
+        order.setTotalAmount(0);
+        order.setCurrency("GBP");
+        order.setDateCreated(new Date());
+        order.setWorldPayStatus("IN_PROGRESS");
+        order.setDateLastUpdated(order.getDateCreated());
+        order.setUpdatedByUser("agent@defra.gsi.gov.uk");
+
+        ArrayList<Order> orders = new ArrayList<Order>();
+        orders.add(order);
+        details.setOrders(orders);
+
+	    details.setBalance(0);
+
+	    return details;
+    }
+
+    private String generateUUID() {
+        String uuid = UUID.randomUUID().toString();
+
+        return Base64
+                .getUrlEncoder()
+                .withoutPadding()
+                .encodeToString(uuid.getBytes(StandardCharsets.UTF_8));
+    }
 	
 }
