1d3a89f36febddddcac15601188aa93edfab6cdf
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-05-09 16:59:50 +0100

    Update test env vars to use test database
    So that we are working with known registrations in the unit tests, each test class will create the registrations it needs then empty (actually delete) the collection once complete.
    
    Because of this pointing to the actual database means it'll be wiped, which can be a pain if it contained registrations you wanted to keep.
    
    So in future there is an assumption that a test database is available and so this change updates the env vars we set before running the tests.

--- test_env_vars.sh
+++ test_env_vars.sh
@@ -4,6 +4,6 @@
 
 export WCRS_SERVICES_DB_HOST="localhost"
 export WCRS_SERVICES_DB_PORT=27017
-export WCRS_SERVICES_DB_NAME="waste-carriers"
+export WCRS_SERVICES_DB_NAME="waste-carriers-test"
 export WCRS_SERVICES_DB_USER="mongoUser"
 export WCRS_SERVICES_DB_PASSWD="password1234"
