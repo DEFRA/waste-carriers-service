72ec4af2f551374475e6bb960dc96759eb9bc053
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-01-27 15:55:53 +0000

    Remove all code related to Worldpay mocks

--- src/main/java/uk/gov/ea/wastecarrier/services/MockConfiguration.java
+++ src/main/java/uk/gov/ea/wastecarrier/services/MockConfiguration.java
@@ -9,21 +9,6 @@ import javax.validation.Valid;
 public class MockConfiguration extends Configuration {
 
     @Valid
-    @NotEmpty
-    @JsonProperty
-    public String worldPayAdminCode;
-
-    @Valid
-    @NotEmpty
-    @JsonProperty
-    public String servicesDomain;
-
-    @Valid
-    @NotEmpty
-    @JsonProperty
-    public String macSecret;
-
-    @Valid
     @JsonProperty
     public Integer delay;
 }
--- src/main/java/uk/gov/ea/wastecarrier/services/resources/MocksResource.java
+++ src/main/java/uk/gov/ea/wastecarrier/services/resources/MocksResource.java
@@ -4,11 +4,8 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.w3c.dom.Document;
 import uk.gov.ea.wastecarrier.services.DatabaseConfiguration;
 import uk.gov.ea.wastecarrier.services.MockConfiguration;
-import uk.gov.ea.wastecarrier.services.core.mocks.WorldpayOrder;
-import uk.gov.ea.wastecarrier.services.dao.MockWorldpayDao;
 import uk.gov.ea.wastecarrier.services.helper.ResourceHelper;
 import uk.gov.ea.wastecarrier.services.helper.mocks.CompaniesHouseHelper;
-import uk.gov.ea.wastecarrier.services.helper.mocks.WorldpayHelper;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -20,54 +17,14 @@ import java.util.logging.Logger;
 @Path("/mocks")
 public class MocksResource {
 
-    private final MockWorldpayDao dao;
     private final CompaniesHouseHelper companyHelper;
-    private final WorldpayHelper worldpayHelper;
     private final Integer mockDelay;
 
     private Logger log = Logger.getLogger(MocksResource.class.getName());
 
     public MocksResource(DatabaseConfiguration configuration, MockConfiguration config) {
-        this.dao = new MockWorldpayDao(configuration);
-        this.worldpayHelper = new WorldpayHelper(config.worldPayAdminCode, config.servicesDomain, config.macSecret);
         this.companyHelper = new CompaniesHouseHelper();
         this.mockDelay = config.delay;
-    }
-
-    @GET
-    @Path("/worldpay/payment-service")
-    public String paymentService(String body) {
-        Document xmlBody = worldpayHelper.stringToXml(body);
-
-        WorldpayOrder order = worldpayHelper.convertFromXml(xmlBody);
-        this.dao.insert(order);
-
-        delay();
-
-        return worldpayHelper.initialResponse(order.merchantCode, order.orderCode, order.worldpayId);
-    }
-
-    @GET
-    @Path("/worldpay/dispatcher")
-    public Response dispatcher(
-            @QueryParam("OrderKey") @NotEmpty String orderKey,
-            @QueryParam("successURL") @NotEmpty String successUrl,
-            @QueryParam("pendingURL") String pendingURL,
-            @QueryParam("failureURL") String failureURL,
-            @QueryParam("cancelURL") String cancelURL,
-            @QueryParam("errorURL") String errorURL
-    ) {
-
-        String orderCode = this.worldpayHelper.extractOrderCodeFromKey(orderKey);
-
-        WorldpayOrder order = this.dao.findByOrderCode(orderCode);
-        String redirectUrl = this.worldpayHelper.orderCompletedRedirectUrl(order, successUrl);
-
-        URI target = UriBuilder.fromUri(redirectUrl).build();
-
-        delay();
-
-        return Response.seeOther(target).build();
     }
 
     @GET
